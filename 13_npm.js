f3968a7a7589e50c482aa15e1be097a05465f9b1
mide18 <ajewoleolumide@gmail.com>
2022-01-26 21:21:10 +0100

    npm update

--- 13_npm.js
+++ 13_npm.js
@@ -18,3 +18,26 @@ const _ = require('lodash');
 const items = [1, [2, [3, [4, [5, [6]]]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+
+
+//to share code with git
+// create .gitignore file...this ignore the files that will be ignored by git fro the source control
+
+//pushing to git
+// git init
+// git add .
+// git commit -m "first commit"
+
+//copy and paste this from git newly created repo
+//git remote add origin https://github.com/mide18/nodejs_intro.git
+//git branch -M main
+//git push -u origin main 
+
+//to clone the project
+// navigate to the folder and copy the project url from git
+// git clone url
+
+//on open the project with editor, if the dependencies are not install
+// run npm install.....npm will check for the dependencies in the package json and install
+
+//control + ` to open terminal
--- app.js
+++
@@ -1,8 +0,0 @@
-const http = require('http');
-
-const creatServer = http.createServer((req, res) => {
-  res.write('Welcome to our homepage');
-  res.end();
-});
-
-creatServer.listen(5000)
